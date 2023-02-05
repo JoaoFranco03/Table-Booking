6069724264fd2278022830dca514244051593e0a
João Franco <joaopedrocostinha1982@gmail.com>
2023-02-05 22:55:49 +0000

    Squashed some bugs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Schedule from './calendar'; 
-import { SchedulerData, ViewTypes, DATE_FORMAT } from 'react-big-scheduler'
 import 'react-big-scheduler/lib/css/style.css'
-import moment from 'moment'
 
 
 function App() {
   render(<Schedule />, document.getElementById('root'));  
+  return null;
 }
 
 export default App;
